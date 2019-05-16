aab26cad8c1c99693e64d53f378e1a3c8d6d5309
ian-jeon <ian@jehmi.com>
2019-05-16 10:11:21 +0900

    Modify stop.sh

--- stop.sh
+++ stop.sh
@@ -4,7 +4,9 @@ export MSYS_NO_PATHCONV=1
 echo "Clearing environment.."
 docker rm -f $(docker ps -aq)
 
-docker-compose -f docker-compose.yml down
+if [ -f "docker-compose.yml" ]; then
+	docker-compose -f docker-compose.yml down
+fi
 
 echo "Removing old chaincode images.."
 docker rmi $(docker images | grep dev-* | awk '{print $3}')
